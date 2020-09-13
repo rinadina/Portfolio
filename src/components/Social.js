352bf7b8005e9569a683bb96b64dc9a7f093adb6
rinadina <rinadina151@gmail.com>
2020-09-13 01:54:41 -0400

    add content pages

---
+++ src/components/Social.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    
+    render() {
+        return(
+            <div className="social">
+                <a href="https://Linkedin.com/in/dharinahanumunthadu" target="_blank"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+        )
+    }
+}
+export default Social
