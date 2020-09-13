275ef23ae437f909387c15c8719d9cb62af4771a
rinadina <rinadina151@gmail.com>
2020-09-13 01:50:51 -0400

    add app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import
 {
 BrowserRouter as Router,
 Route,
-} from "react-router-dom";
-import Navbar from './components/Navbar';
+} from "react-router-dom"
+import Navbar from './components/Navbar'
+import Home from './contents/Home'
+import Contact from './contents/Contact'
+import About from './contents/About'
+import Education from './contents/Education'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/education">
+          <Education />
+        </Route>
       </div>
     </Router>
   )
