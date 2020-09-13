28f799d9185b740a1d5fce3c1a72a06af532dbc5
rinadina <rinadina151@gmail.com>
2020-09-13 02:14:22 -0400

    add alt txt for img

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-                <img src={profilepic} className="profilepic"></img>
+                <img src={profilepic} alt="This is me." className="profilepic"></img>
                 <br></br>
                 <ReactTypingEffect className="typingeffect" text={['Hello, my name is Dharina Hanumunthadu. I am a 4th year in Software Engineering.']} speed={105} eraseDelay={100}/>
                 <Social />
