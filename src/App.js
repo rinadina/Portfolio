2d81c72d9f79fab3ec051522ed3da8789a30d4e5
Dharina H <rinadina151@gmail.com>
2020-09-13 02:30:26 -0400

    Rinadina/issue 1: Add components (#2)
    * adding structure folders components and content
    
    * modify app css and js
    
    * add navbar component
    
    * add app js
    
    * add home page
    
    * add components
    
    * add content pages
    
    * add comment to NavItem
    
    * add rel to links
    
    * add alt txt for img
    
    * rm test for now
    
    * add placeholder for test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import
+{
+BrowserRouter as Router,
+Route,
+} from "react-router-dom"
+import Navbar from './components/Navbar'
+import Home from './contents/Home'
+import Contact from './contents/Contact'
+import About from './contents/About'
+import Education from './contents/Education'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/education">
+          <Education />
+        </Route>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
