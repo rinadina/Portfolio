64366a37fbdb2dbd65aed01068e1719213852fc0
rinadina <rinadina151@gmail.com>
2020-09-13 21:00:44 -0400

    Updates

---
+++ precache-manifest.e5b258a868b5f732905637601b1e059b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b5199235417de85b64311374b8c966",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e3c599fa4b16fa80453b",
+    "url": "/Portfolio/static/css/main.1028f85c.chunk.css"
+  },
+  {
+    "revision": "4f0712c9ff8227628485",
+    "url": "/Portfolio/static/js/2.ab564629.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/Portfolio/static/js/2.ab564629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c599fa4b16fa80453b",
+    "url": "/Portfolio/static/js/main.fb74ea29.chunk.js"
+  },
+  {
+    "revision": "95694ffb7194c73ef8f9",
+    "url": "/Portfolio/static/js/runtime-main.5ac5bc46.js"
+  },
+  {
+    "revision": "38faf5bc39c557ae3debd06e79cf0064",
+    "url": "/Portfolio/static/media/professional-picture.38faf5bc.jpg"
+  }
+]);
